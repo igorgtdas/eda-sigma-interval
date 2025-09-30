24f00820bafb5b84181a55d914af61783d7412c8
Igor Gonçalves <152019458+igorgtdas@users.noreply.github.com>
2025-09-30 15:10:27 -0300

    Update exemplo_de_uso.py

--- scripts/exemplo_de_uso.py
+++ scripts/exemplo_de_uso.py
@@ -4,12 +4,7 @@ from eda_igor import assess_normality, outlier_summary, sigma_intervals, run_eda
 
 # --------------------- Exemplo de uso -----------------------
 if __name__ == "__main__":
-    # 1) Rodar em um CSV local:
-    # df = pd.read_csv("seu_arquivo.csv")
-    # run_eda(df, dataset_name="seu_arquivo.csv")
-
-    # 2) Ou detectar automaticamente o maior CSV do diretório atual:
-    path = 'YOURPATH+YOUR_FILE'
-    df = pd.read_excel(path)
+    file = 'your_file.xlsx'    #-- Trocar para o nome do seu arquivo
+    df = pd.read_excel(file)
     
     run_eda(df, dataset_name=os.path.basename(path))
